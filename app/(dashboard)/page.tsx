35ef4c1878272e2385b6c737ca6e14cf944ef52d
Trung Nghia Pham <fb.trungnghia13@gmail.com>
2024-04-29 14:46:19 +0700

    fixed search error

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,16 +2,13 @@
 import { BoardList } from "./_components/board-list";
 import { EmptyOrg } from "./_components/empty-org";
 import { useOrganization } from "@clerk/nextjs";
-interface DashboardPageProps {
-    searchParams: {
-        search?: string;
-        favorites?: string;
-    };
-}
-const DashboardPage = ({
-    searchParams,
-}: DashboardPageProps) => {
+import { useSearchParams } from "next/navigation";
+
+const DashboardPage = () => {
     const { organization } = useOrganization();
+    const searchParams = useSearchParams();
+    const search = searchParams.get("search") || undefined;
+    const favorites = searchParams.get("favorites")|| undefined;
     return (
         <div className=" flex-1 h-[calc(100%-80px)] p-6">
             
@@ -21,7 +18,7 @@ const DashboardPage = ({
             ) : (
                 <BoardList
                     orgId={organization.id}
-                    query={searchParams}
+                    query={{ search,favorites }}
                 />
             )
             }
